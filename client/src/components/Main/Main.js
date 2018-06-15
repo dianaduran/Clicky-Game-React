37732d0d44fdefcc3c226441d7c8eb9646e29476
Diana Duran <dianaduran17@gmail.com>
2018-06-15 13:25:20 -0400

    ramdom img

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -5,24 +5,44 @@ import Img from "../Img";
 
 import { Container } from "../../components/Grid";
 
+
+ function shuffleArray (array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  console.log(array);
+ return array;
+};
+
 class Main extends Component {
 
   state = {
     dogs
   };
-  
+
+  // shuffledDogs=()=>{
+
+  //   return shuffleArray(dogs);
+   
+  // };
+ 
   render() {
+    let shuffledDogs = shuffleArray(this.state.dogs);
+  
     return (
-    <Container>
-    {this.state.dogs.map(dog => (
+   <Container>
+      {shuffledDogs.map((dog, key) => (
         <Img
             id={dog.id}
             key={dog.id}
             url={dog.url}
            />
         ))}
-    </Container>
-  
+    </Container>  
   );
  }
 }
