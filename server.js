ffebb6fb054411af3e5934b911913479d4b3a1ba
Diana Duran <dianaduran17@gmail.com>
2018-06-18 10:37:37 -0400

    working in server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, 'client/build')));
   }
 
-  app.get("*", function(req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 
