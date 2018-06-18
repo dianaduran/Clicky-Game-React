6301bd252b39e5f289608e9cfbcdd81a5e635048
Diana Duran <dianaduran17@gmail.com>
2018-06-18 10:29:22 -0400

    path file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -6,6 +7,10 @@ const port = process.env.PORT || 3001;
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
