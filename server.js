7ed9bb1caaee2e21c29aaa5ceab7df38bfb92fea
Diana Duran <dianaduran17@gmail.com>
2018-06-18 10:19:34 -0400

    conf

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
